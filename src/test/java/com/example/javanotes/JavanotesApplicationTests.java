564f73c732bf4fb6eb602ba88e113e4ca3cf25f4
nikhilchintawar <chintawarnikhil88@gmail.com>
2024-06-12 21:31:18 +0530

    First commit

---
+++ src/test/java/com/example/javanotes/JavanotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javanotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavanotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
